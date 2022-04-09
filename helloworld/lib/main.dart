1d011278d63e207d0043aea16b5a8f27d3f859eb
Paulo Antonio Mendes <thelukgaming333@gmail.com>
2022-04-09 12:00:28 -0300

    Setting up the project

---
+++ helloworld/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container(
+    child: Text(
+      'Meu Primeiro aplicativo Flutter',
+      textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 50, color: Colors.blue),
+    ),
+  ));
+}
