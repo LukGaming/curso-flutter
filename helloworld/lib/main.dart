b7748794d239192c4da1a982c2e2214be83e1ee6
Paulo Antonio Mendes <thelukgaming333@gmail.com>
2022-04-10 11:03:38 -0300

    Primeiros Inputs de Formulários

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -1,12 +1,103 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Container(
-    child: Text(
-      'Meu Primeiro aplicativo Flutter',
-      textDirection: TextDirection.ltr,
-      style: TextStyle(fontSize: 50, color: Colors.blue),
-    ),
-  ));
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Primeiro Formulário';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final passwordController = TextEditingController();
+  final emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Align(
+              alignment: Alignment
+                  .center, // Align however you like (i.e .centerRight, centerLeft)
+              child:
+                  Text("Cadastro de Usuários", style: TextStyle(fontSize: 20)),
+            ),
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: "Endereco de email"),
+              keyboardType: TextInputType.emailAddress,
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            Padding(padding: const EdgeInsets.symmetric(vertical: 5.0)),
+            TextFormField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                  // hintText: 'Senha',
+                  labelText: "Senha"),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        // Retrieve the text the that user has entered by using the
+                        // TextEditingController.
+                        content: Text(
+                            " EmaiL: \n ${emailController.text} \n Senha:  \n ${passwordController.text}"),
+                      );
+                    },
+                  );
+                  // Validate will return true if the form is valid, or false if
+                  // the form is invalid.
+                  if (_formKey.currentState!.validate()) {
+                    // Process data.
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
