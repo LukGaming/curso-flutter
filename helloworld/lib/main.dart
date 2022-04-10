55f7ee67dace4de6f28e321809312802c4faf87b
Paulo Antonio Mendes <thelukgaming333@gmail.com>
2022-04-10 11:23:34 -0300

    Criando primeira Interfacew de usuário

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(const MyApp());
 
@@ -19,6 +22,26 @@ class MyApp extends StatelessWidget {
   }
 }
 
+Future<User> createUser(String email, String password) async {
+  final response = await http.post(
+    Uri.parse('http://localhost:3000/users'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{'email': email, 'password': password}),
+  );
+
+  if (response.statusCode == 201) {
+    // If the server did return a 201 CREATED response,
+    // then parse the JSON.
+    return User.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 201 CREATED response,
+    // then throw an exception.
+    throw Exception('Failed to create User.');
+  }
+}
+
 class MyStatefulWidget extends StatefulWidget {
   const MyStatefulWidget({Key? key}) : super(key: key);
 
@@ -30,6 +53,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final passwordController = TextEditingController();
   final emailController = TextEditingController();
+  Future<User>? _futureAlbum;
 
   @override
   Widget build(BuildContext context) {
@@ -75,6 +99,9 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: ElevatedButton(
                 onPressed: () {
+                  setState(() {
+                    _futureAlbum = createUser(emailController.text, passwordController.text);
+                  });
                   showDialog(
                     context: context,
                     builder: (context) {
@@ -86,10 +113,12 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                       );
                     },
                   );
+
                   // Validate will return true if the form is valid, or false if
                   // the form is invalid.
                   if (_formKey.currentState!.validate()) {
                     // Process data.
+
                   }
                 },
                 child: const Text('Submit'),
@@ -99,5 +128,14 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         ),
       ),
     );
+  }
+}
+
+class User {
+  final String email;
+  final String password;
+  const User({required this.email, required this.password});
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(email: json['email'], password: json['password']);
   }
 }
